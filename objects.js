82a33bdfaa0c62c7bfa1aa8ce13cc9d5e8be4033
Matthew Korzen <mattkorzen14@gmail.com>
2018-08-10 05:41:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist;
 }
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+  console.log(delete playlist.artistName);
   console.log(playlist + "ywo");
   return playlist;
 
@@ -13,9 +13,3 @@ function removeFromPlaylist(playlist, artistName) {
 
 updatePlaylist(playlist, "These Guys", "I'll Be Fine")
 removeFromPlaylist(playlist, "Maroon 5");
-
-var meals = { breakfast: "oatmeal", lunch: "turkey sandwich", dinner: "steak and potatoes" };
-
-delete meals.dinner; // true
- 
-console.log(meals);
