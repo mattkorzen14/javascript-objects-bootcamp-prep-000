dad754309f8b171fd3e720d1f0c58aad04a4543e
Matthew Korzen <mattkorzen14@gmail.com>
2018-08-10 05:34:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {"Maroon 5": "Sunday Morning"};
+playlist = {"Maroon 5": "Sunday Morning"};
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
   console.log(playlist)
