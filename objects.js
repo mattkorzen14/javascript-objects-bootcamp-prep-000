fcfd70b5ca7a3a136b7a7fba903119aeca317fe7
Matthew Korzen <mattkorzen14@gmail.com>
2018-08-10 05:51:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,8 +5,9 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName
+  //console.log(playlist)
   return playlist;
 }
 
-updatePlaylist(playlist, "These Guys", "I'll Be Fine")
-removeFromPlaylist(playlist, "Maroon 5");
+//updatePlaylist(playlist, "These Guys", "I'll Be Fine")
+//removeFromPlaylist(playlist, "Maroon 5");
