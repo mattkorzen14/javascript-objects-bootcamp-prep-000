d0a92525501cd2a9e5b18bedf1e361ceb39fd1f1
Matthew Korzen <mattkorzen14@gmail.com>
2018-08-10 05:35:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -13,3 +13,11 @@ function removeFromPlaylist(playlist, artistName) {
 
 updatePlaylist(playlist, "These Guys", "I'll Be Fine")
 removeFromPlaylist(playlist, "Maroon 5");
+
+var meals = { breakfast: "oatmeal", lunch: "turkey sandwich", dinner: "steak and potatoes" };
+ 
+// the `delete` operator returns `true` if it has successfully
+// deleted, `false` otherwise
+delete meals.dinner; // true
+ 
+meals;
