60280664c502b9c017d6341dd45d9369dbf13f76
Matthew Korzen <mattkorzen14@gmail.com>
2018-08-10 05:42:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 function removeFromPlaylist(playlist, artistName) {
   console.log(delete playlist.artistName);
+  delete playlist.artistName
   console.log(playlist);
   return playlist;
 
